c742aaef6a0188765c4cef2824777f7437581137
devincloudkelly <devin.cloud.kelly@gmail.com>
2020-01-29 11:23:59 -0800

    added namespaces to routes and controllers

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
+    has_secure_password
+    validates: :email_address, uniqueness: { case_sensitive: false }
     has_many :preparations
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  resources :steps
-  resources :preparations
-  resources :users
+  namespace :api do
+    namespace :v1 do
+      resources :steps
+      resources :preparations
+      resources :users
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
