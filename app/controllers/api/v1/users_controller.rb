87e67ddbdb85494eb1aff0697c58784820d55ba4
devincloudkelly <devin.cloud.kelly@gmail.com>
2020-01-30 09:56:16 -0800

    changed user show route to profile

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::UsersController < ApplicationController
     skip_before_action :authorized, only: [:create]
 
-    def show
+    def profile
         user = User.find_by(id: params[:id])
         render json: user, status: :accepted
     end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
       resources :steps
       resources :preparations
       resources :users
+      get '/profile' to: 'users#profile'
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
