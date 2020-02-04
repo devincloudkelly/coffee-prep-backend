397ebbb8bae01f07154db3ca1cc62099d777fc20
devincloudkelly <devin.cloud.kelly@gmail.com>
2020-02-04 09:35:51 -0800

    added nested steps in active model serializer for preparations

--- config/application.rb
+++ config/application.rb
@@ -27,7 +27,7 @@ module CoffeePrepBackend
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', headers: :any, methods: [:get, :post, :options]
+        resource '*', headers: :any, methods: [:get, :post, :patch, :put, :delete, :options]
       end
     end
     # Settings in config/environments/* take precedence over those specified here.
