6fba9a134ed2f011eccb4bbd5c1cc59b55cfe9d3
devincloudkelly <devin.cloud.kelly@gmail.com>
2020-02-04 10:15:42 -0800

    updated serializer

--- app/serializers/preparation_serializer.rb
+++ app/serializers/preparation_serializer.rb
@@ -2,5 +2,5 @@ class PreparationSerializer < ActiveModel::Serializer
   attributes :id, :device, :coffee_brand, :coffee_name, :coffee_amount, :coffee_grind, :total_water, :total_time, :water_temp, :notes
   has_one :user
   has_many :steps
-  # ActiveModelSerializers.config.default_includes = '**'
+  
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
+  ActiveModelSerializers.config.default_includes = '**'
   attributes :id, :name, :email_address, :password, :has_aeropress, :has_chemex, :has_pourover
+  has_many :preparations
 end
